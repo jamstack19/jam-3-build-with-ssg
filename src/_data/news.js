56c76e99de9c3310f3a384338722697e3ee8dd84
Benjamin Le Goff <benji5blg@gmail.com>
2021-09-09 07:37:02 +0200

    rewrites

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,12 +1,27 @@
 const axios = require('axios');
+const countries = require('./countries.json');
+
 require('dotenv').config();
+
+async function getNews(country) {
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    return {
+      "country": country,
+      "articles": response.data.articles
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // Want to use async/await? Add the `async` keyword to your outer function/method.
 module.exports = async function() {
-    try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+
+  let newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then(newsObjects =>{
+    console.log('newsObjects:', newsObjects);
+    return [].concat.apply([], newsObjects);
+  })
+  
+};
